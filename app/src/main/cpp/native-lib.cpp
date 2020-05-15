6ad48c7e25e0ecf6596ad2d0de948c5b30d8dd8e
wangfayouy <f_y_wang6@163.com>
2020-05-15 08:18:12 +0800

    update3

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,11 +1,15 @@
 #include <jni.h>
 #include <string>
+#include <cstring>
 #include "Test.h"
 #include <android/log.h>
 
 using namespace std;
 
-
+#define LOGI(FORMAT, ...) __android_log_print(ANDROID_LOG_INFO,"tag",FORMAT,##__VA_ARGS__);
+#define LOGE(FORMAT, ...) __android_log_print(ANDROID_LOG_ERROR,"tag",FORMAT,##__VA_ARGS__);
+#define LOGD(FORMAT, ...) __android_log_print(ANDROID_LOG_DEBUG,"tag",FORMAT,##__VA_ARGS__);
+#define LOGV(FORMAT, ...) __android_log_print(ANDROID_LOG_VERBOSE,"tag",FORMAT,##__VA_ARGS__);
 
 
 extern "C"
@@ -18,5 +22,6 @@ extern "C"
 JNIEXPORT jstring JNICALL
 Java_com_youbesun_myapplication_NDKTool_stringFromJNI(JNIEnv *env, jclass clazz) {
     string hello = test();
+    LOGE("%s", hello.c_str());
     return env->NewStringUTF(hello.c_str());
 }
